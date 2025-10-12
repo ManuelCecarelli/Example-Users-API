fd06e6cfffc14f48393295e018e58b2086903092
ManuelCecarelli <manuelcecarelli@gmail.com>
2025-10-12 12:53:39 -0300

    Add User entity and enums on Domain layer

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+        public DateTime LastUpdate { get; set; }
+        public Status Status { get; set; } = Status.Active;
+        public UserType Role { get; set; } = UserType.Client;
+    }
+}
