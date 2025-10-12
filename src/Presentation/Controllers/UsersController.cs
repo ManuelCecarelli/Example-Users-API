5c7f76b70782e0ea7d153cd993ee92c85a1c4e9a
ManuelCecarelli <manuelcecarelli@gmail.com>
2025-10-12 16:02:03 -0300

    Add UsersController

---
+++ src/Presentation/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserDTO>>> GetUsers()
+        {
+            return await _userService.GetUsers();
+        }
+    }
+}
