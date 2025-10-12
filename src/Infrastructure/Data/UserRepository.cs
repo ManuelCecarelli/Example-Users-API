ee0ae31cbc70cf88a28af8ff41a962cc0223d2e5
ManuelCecarelli <manuelcecarelli@gmail.com>
2025-10-12 15:24:29 -0300

    Add UserRepository

---
+++ src/Infrastructure/Data/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _context.Users.ToListAsync();
+        }
+    }
+}
