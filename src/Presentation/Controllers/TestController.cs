44227d38dd4797d2166edb64d54a2439e4232113
ManuelCecarelli <manuelcecarelli@gmail.com>
2025-10-15 16:38:07 -0300

    Add TestController

---
+++ src/Presentation/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult<string> TestNoRoleEndpoint()
+        {
+            return "Accediste al endpoint protegido general(nivel de cliente)";
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Employee")]
+        public ActionResult<string> TestEmployeeEndpoint()
+        {
+            return "Accediste al endpoint de nivel de empleado";
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public ActionResult<string> TestAdminEndpoint()
+        {
+            return "Accediste al endpoint de nivel de administrador";
+        }
+    }
+}
